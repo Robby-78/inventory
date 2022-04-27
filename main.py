51559e9fdde593fed46da4c1a7b5d2cc6e9f5a3f
=Robby-78 <robinsonkoech78@gmail.com>
2022-04-27 21:34:52 +0300

    third commit

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ conn = psycopg2.connect("dbname='dcq2mm2ag4dcdn' user='qhdfbpkgxbngyh' host='ec2
 # except:
 #     print ("I am unable to connect to the database")
 cur = conn.cursor()
-cur.execute("""CREATE TABLE IF NOT EXISTS products (id serial PRIMARY KEY, name VARCHAR (100) NOT NULL, buying_price VARCHAR (30)  NOT NULL, selling_price VARCHAR (30) NOT NULL, stock_quantity VARCHAR (30) NOT NULL);""")
+cur.execute("""CREATE TABLE IF NOT EXISTS products (id serial PRIMARY KEY, name VARCHAR (100) NOT NULL, buying_price VARCHAR (30)  NOT NULL, selling_price VARCHAR (30) NOT NULL, stock_quantity VARCHAR (30) NOT NULL), primary key(id);""")
 cur.execute("""CREATE TABLE IF NOT EXISTS sales (id serial PRIMARY KEY,pid integer REFERENCES products(id), quantity VARCHAR (30) NOT NULL, created_at VARCHAR (30) NOT NULL);""")
 conn.commit()
 
